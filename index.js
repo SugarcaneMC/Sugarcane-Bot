383ac835f0979dcd6527de43039b0ff5834ed6cf
Chris Chrome <christophercookman@gmail.com>
2021-07-02 22:21:16 -0400

    Add autorole and message handling

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}`);
 })
 
+bot.on('guildMemberAdd', (member) => {
+    switch(member.guild.id) {
+        case "855918593497759754":
+            member.roles.add("855981133731856414")
+            break;
+    }
+})
+
+bot.on('message', (msg) => {
+    const prefix = config.discord.prefix;
+    const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (msg.content.toLowerCase().startsWith(prefix)) {
+        switch (cmd) {
+            
+        }
+    }
+})
+
 bot.login(config.discord.token);
